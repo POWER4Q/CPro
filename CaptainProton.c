582d9f372fee0cd8ff2f01437dd87c9e6fd66bc4
POWER4Q <148098432+POWER4Q@users.noreply.github.com>
2024-01-06 19:27:30 -0500

    Create CaptainProton.c

---
+++ CaptainProton.c
@@ -0,0 +1,13 @@
+// I decided to port this game over to C, since I could not get the people that made the Basic to make improvements to their language
+// I am going to communicate with a language of my own
+// The Basic sucks since it does not work properly on a new device that was released a few months ago.
+#include "prosystem.h"
+#include "multisprite_special.h"
+#include "joystick.h"
+#include "string.h"
+#include "sfx.h"
+#include "qspecial1.h"
+#include "qspecial2.h"
+#define BytesPerRow             40
+#define PlexMax                 20
+#define First_Opponent_Sprite   4
